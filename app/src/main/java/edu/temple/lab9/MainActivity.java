cc2c6dc31dc86bef0715376d7c17c2f7fbac7d45
garrbows <gbowser3@gmail.com>
2021-04-16 19:38:45 -0400

    added search button, implemented service binder, play/pause buttons:

--- app/src/main/java/edu/temple/lab9/MainActivity.java
+++ app/src/main/java/edu/temple/lab9/MainActivity.java
@@ -8,6 +8,9 @@ import android.os.Binder;
 import android.os.Bundle;
 import android.os.IBinder;
 import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,31 +22,44 @@ public class MainActivity extends AppCompatActivity {
     AudiobookService.MediaControlBinder player;
     boolean bound = false;
 
+    int nowPlaying = -1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
 
-        Intent intent = new Intent(this, BoundAudiobookService.class);
+        Intent intent = new Intent(this, AudiobookService.class);
         bindService(intent, mConnection, Context.BIND_AUTO_CREATE);
 
-        int i=0;
-        while(i<100000) {
-            i++;
-        }
+        Button searchButton = (Button) findViewById(R.id.searchButton);
+        Button playButton = (Button) findViewById(R.id.playButton);
+        Button pauseButton = (Button) findViewById(R.id.pauseButton);
 
-        if(bound) {
-            player.play(300);
-        } else {
-            Context context = getApplicationContext();
-            CharSequence text = "Bind failed";
-            int duration = Toast.LENGTH_SHORT;
+        searchButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                TextView search = findViewById(R.id.input);
+                String id = search.getText().toString();
+                player.play(Integer.valueOf(id));
+                nowPlaying = Integer.valueOf(id);
+            }
+        });
 
-            Toast toast = Toast.makeText(getApplicationContext(), text, Toast.LENGTH_SHORT);
-            toast.show();
+        playButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                player.play(nowPlaying);
+            }
+        });
 
-        }
+        pauseButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                player.pause();
+            }
+        });
+
     }
+
+
 
     @Override
     protected void onStart() {
@@ -59,8 +75,8 @@ public class MainActivity extends AppCompatActivity {
                                        IBinder service) {
             AudiobookService.MediaControlBinder binder = (AudiobookService.MediaControlBinder) service;
             player = binder;
+            Toast.makeText(getApplicationContext(), "Bind successful", Toast.LENGTH_SHORT).show();
             bound = true;
-
         }
 
         @Override
@@ -68,5 +84,6 @@ public class MainActivity extends AppCompatActivity {
             bound = false;
         }
     };
+
 
 }
